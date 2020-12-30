7617daf78e794ea4fc941f95a7a77e0eade70706
kimxwan0319 <kimxwan0319@naver.com>
2020-12-30 10:52:04 +0900

    set file

---
+++ Socket.IO_test/Socket.IO_test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Socket.IO_test
+//
+//  Created by 김수완 on 2020/12/21.
+//
+
+import UIKit
+import SocketIO
+
+class ViewController: UIViewController {
+
+    var socket : SocketIOClient!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
+class SocketIOManager: NSObject{
+    
+}
